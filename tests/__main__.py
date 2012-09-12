61bbbe320d765a9be4207f6cdd80f9f1d65f18b3
remram44 <remirampin@gmail.com>
2012-09-11 22:40:46 -0400

    Test runner and folder.

---
+++ tests/__main__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import unittest
+
+
+top_level = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+start_dir = os.path.join(top_level, 'tests')
+if not top_level in sys.path:
+    sys.path.append(top_level)
+
+
+class Program(unittest.TestProgram):
+    def createTests(self):
+        # Is there really no way to load the tests from the package, using
+        # TestLoader#discover(), while still using main()?
+        # Why would you provide TestLoader, TestRunner and all that nonsense if
+        # I can't do that without overriding part of main itself?
+        if self.testNames is None:
+            self.test = tests = self.testLoader.discover(
+                    start_dir=start_dir,
+                    pattern='test_*.py',
+                    top_level_dir=top_level)
+        else:
+            self.test = self.testLoader.loadTestsFromNames(self.testNames)
+
+Program()
---
+++ tests/internals/__init__.py
@@ -0,0 +1,6 @@
+"""Tests for the _pyjava module.
+
+This package contains tests for the internal components of PyJava, implemented
+in C as the _pyjava package. Even if it is not used directly but rather through
+the pyjava Python interface, the package is covered by tests.
+"""
